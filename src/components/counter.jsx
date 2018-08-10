2cb2dc2f351b0fb5c5d54eaf1ff7c2e0de4477aa
gustavo32 <luisouza98@gmail.com>
2018-08-09 22:54:45 -0300

    Add some features and style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,55 @@ import React, { Component } from "react";
 
 class Counter extends React.Component {
 	state = {
-		value: this.props.value
+		value: this.props.counter.value
 	};
 
 	handleIncrement = () => {
 		this.setState({ value: this.state.value + 1 });
 	};
 
+	handleDecrement = () => {
+		this.setState({ value: this.state.value - 1 });
+		if (this.state.value === 0) {
+		}
+	};
+
 	render() {
+		const circleStyle = {
+			borderRadius: 18,
+			textAlign: "center",
+			padding: "6 0",
+			margin: 1,
+			lineHeight: 1.42,
+			fontSize: 12,
+			witdh: 30,
+			height: 30
+		};
+
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button
-					className="btn btn-outline-success btn-sm"
+					className="btn btn-outline-success"
 					onClick={this.handleIncrement}
+					style={circleStyle}
 				>
-					Increment
+					<p style={{ fontWeight: "bold" }}>+</p>
+				</button>
+				<button
+					className="btn btn-secondary"
+					onClick={this.handleDecrement}
+					style={circleStyle}
+					disabled={!this.state.value}
+				>
+					<p style={{ fontWeight: "bold" }}>-</p>
+				</button>
+				<button
+					className="btn btn-danger ml-5"
+					style={circleStyle}
+					onClick={() => this.props.onDelete(this.props.counter.id)}
+				>
+					<p style={{ fontWeight: "bold" }}>&times;</p>
 				</button>
 			</div>
 		);
@@ -32,10 +65,6 @@ class Counter extends React.Component {
 	formatCount() {
 		const { value } = this.state;
 		return value === 0 ? "Zero" : value;
-	}
-
-	doIncrement() {
-		return this.setState({ value: 1 });
 	}
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -6,11 +6,20 @@ class Counters extends React.Component {
 		counters: [{ id: 0, value: 0 }, { id: 1, value: 1 }, { id: 2, value: 2 }]
 	};
 
+	handleDelete = counterId => {
+		const counters = this.state.counters.filter(c => c.id !== counterId);
+		this.setState({ counters: counters });
+	};
+
 	render() {
 		return (
 			<div>
 				{this.state.counters.map(counter => (
-					<Counter key={counter.id} value={counter.value} />
+					<Counter
+						key={counter.id}
+						onDelete={this.handleDelete}
+						counter={counter}
+					/>
 				))}
 			</div>
 		);
